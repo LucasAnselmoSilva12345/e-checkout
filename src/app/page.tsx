d49b8be86d5ed57c3f91ef515a3ee4efa49202d7
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-10-20 21:09:49 -0300

    feat: base of project
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/layout/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className=""></main>
+    </div>
+  );
+}
---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="p-4 border-b border-b-stone-200">
+      <div className="lg:max-w-[1330px] lg:my-0 lg:mx-auto">
+        <h1 className="font-bold text-2xl text-stone-900">E-Checkout</h1>
+      </div>
+    </header>
+  );
+}
